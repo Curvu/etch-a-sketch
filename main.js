2573f175e556af63dbd47ca263e0da06a4150e36
Curvu <rodrigues.filipealexandre2003@gmail.com>
2022-08-12 13:07:44 +0100

    function to allow paint each square

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const container = document.querySelector('.container');
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+
+function paint(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.backgroundColor = 'black'
+}
+
 function createGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+
     let gridElement;
     for (let i = 0; i < (size*size); i++) {
         gridElement = document.createElement('div');
         gridElement.classList.add('grid-element');
-        // grid-template-columns: repeat(16, 1fr);
-        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+        gridElement.addEventListener('mouseover', paint)
+        gridElement.addEventListener('mousedown', paint)
         container.appendChild(gridElement);
     }
 }
