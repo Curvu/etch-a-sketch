93b8870bf620f42dbaeb007b3a089c07cc0c40bc
Curvu <rodrigues.filipealexandre2003@gmail.com>
2022-08-13 15:56:43 +0100

    function to resize the board with the input range value

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const lines = document.getElementById('lines');
 lines.addEventListener('click', () => {
     for (let i = 0; i < (SIZE*SIZE); i++) {
         grid.children[i].classList.toggle('show-line');
-    }
+    };
 });
 
-// Function that reset the board
+// Reset the board
 const reset = document.getElementById('reset');
 reset.addEventListener('click', () => {
     grid.innerHTML = '';
@@ -21,24 +21,37 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+// Paint the square
 function paint(e) {
-    if (e.type === 'mouseover' && !mouseDown) return
-    e.target.style.backgroundColor = 'black'
-}
+    if (e.type === 'mouseover' && !mouseDown) return;
+    e.target.style.backgroundColor = 'black';
+};
 
+// Create the board
 function createGrid(size) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
     let gridElement;
     for (let i = 0; i < (size*size); i++) {
         gridElement = document.createElement('div');
         gridElement.classList.add('grid-element');
-        gridElement.addEventListener('mouseover', paint)
-        gridElement.addEventListener('mousedown', paint)
+        gridElement.addEventListener('mouseover', paint);
+        gridElement.addEventListener('mousedown', paint);
         grid.appendChild(gridElement);
-    }
-}
+    };
+};
 
+// Slider
+const slider = document.querySelector('.slider');
+const value = document.getElementById('slider-val');
+slider.addEventListener('input', (e) => {
+    value.textContent = e.currentTarget.value;
+    SIZE = e.currentTarget.value;
+    grid.innerHTML = '';
+    createGrid(SIZE);
+});
+
+// After load
 window.onload = () => {
-    createGrid(SIZE)
-}
+    createGrid(SIZE);
+};
