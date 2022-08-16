77ab9690e0359d7a4b626f3985a55ad5fab76720
Curvu <rodrigues.filipealexandre2003@gmail.com>
2022-08-16 20:30:34 +0100

    changing vars and const position, reset now will reset size and button state

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 const grid = document.querySelector('.grid');
-let SIZE = 16;
-
-let color = 'black';
-
-// Shows the line of each square
 const lines = document.getElementById('lines');
-lines.addEventListener('click', () => {
-    for (let i = 0; i < (SIZE*SIZE); i++) {
-        grid.children[i].classList.toggle('show-line');
-    };
-});
-
-// Reset the board
 const reset = document.getElementById('reset');
-reset.addEventListener('click', () => {
-    grid.innerHTML = '';
-    createGrid(SIZE);
-});
+const slider = document.querySelector('.slider');
+const value = document.getElementById('slider-val');
+const rainbowBtn = document.getElementById('rainbow');
+const colorInput = document.getElementById('color-input');
 
 // Check if mouse is Up or Down
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
+
+let rainbowToggler = false;
+
+let SIZE = 16;
+let color = 'black';
+
+// Shows the line of each square
+lines.addEventListener('click', () => {
+    for (let i = 0; i < (SIZE*SIZE); i++) {
+        grid.children[i].classList.toggle('show-line');
+    };
+    lines.classList.toggle('active');
+});
+
+// Reset the board
+reset.addEventListener('click', () => {
+    // Reset the size
+    SIZE = 16;
+    value.textContent = 16;
+    slider.value = 16;
+
+    // Clean grid and re-create
+    grid.innerHTML = '';
+    createGrid(SIZE);
+
+    // Check if grid lines button is active or not
+    if (lines.classList.contains('active')) return;
+    else lines.classList.add('active');
+});
 
 // Paint the square
 function paint(e) {
@@ -38,6 +55,7 @@ function createGrid(size) {
     for (let i = 0; i < (size*size); i++) {
         gridElement = document.createElement('div');
         gridElement.classList.add('grid-element');
+        gridElement.classList.add('show-line');
         gridElement.addEventListener('mouseover', paint);
         gridElement.addEventListener('mousedown', paint);
         grid.appendChild(gridElement);
@@ -45,8 +63,6 @@ function createGrid(size) {
 };
 
 // Slider
-const slider = document.querySelector('.slider');
-const value = document.getElementById('slider-val');
 slider.addEventListener('input', (e) => {
     value.textContent = e.currentTarget.value;
     SIZE = e.currentTarget.value;
@@ -55,9 +71,6 @@ slider.addEventListener('input', (e) => {
 });
 
 // Paint rainbow
-const rainbowBtn = document.getElementById('rainbow');
-
-let rainbowToggler = false;
 rainbowBtn.addEventListener('click', () => {
     rainbowToggler = !rainbowToggler;
     rainbowBtn.classList.toggle('active');
@@ -72,17 +85,16 @@ function rainbow() {
     colorInput.value = rgbToHex(r, g, b);
 };
 
-// Color input
+// Convert RGB to HEX
 function componentToHex(c) {
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
 };
-
 function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 };
 
-const colorInput = document.getElementById('color-input');
+// Color input
 colorInput.addEventListener('input', (e) => {
     color = colorInput.value;
 });
